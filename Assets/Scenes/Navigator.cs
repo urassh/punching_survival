e6702c48f676dc6e368d1ac7d27a20bd3bfd263b
urassh <shuto.1011@icloud.com>
2025-09-11 16:34:59 +0900

    feat: navigator

---
+++ Assets/Scenes/Navigator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Navigator : MonoBehaviour
+{
+    public void NavigateToStart()
+    {
+        LoadScene("Start");
+    }
+
+    public void NavigateToLobby()
+    {
+        LoadScene("Lobby");
+    }
+
+    public void NavigateToLobbyJoin()
+    {
+        LoadScene("LobbyJoin");
+    }
+
+    public void NavigateToPlay()
+    {
+        LoadScene("Play");
+    }
+
+    public void NavigateToResult()
+    {
+        LoadScene("Result");
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
