edef82d5b0ede69f34a081ead048dabf254d3183
hminemur0x0 <hminemur@student.42tokyo.jp>
2025-09-18 23:47:53 +0900

    入力したルーム名からルームを探して入室する機能を追加 (#13)
    * 入力したルーム名からルームを探して入室する機能を追加
    
    * stash
    
    * ルームを検索して接続する機能を追加
    
    ---------
    
    Co-authored-by: unknown <h.minemura.0x@gmail.com>

--- Assets/Scenes/CreateRoom.cs
+++ Assets/Scenes/CreateRoom.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Fusion;
 
 public class CreateRoom : MonoBehaviour
 {
     public NetworkRunner networkRunnerPrefab;
-    private async void Start()
-    {
-        //prefab(ゲームオブジェクトの設計書)を使ってゲームオブジェクトをコピーする
-        var networkRunner = Instantiate(networkRunnerPrefab);
-        //部屋を作る
-        var result = await networkRunner.StartGame(new StartGameArgs
-        {
-            SessionName = RoomNumberText.roomNumStr,
-            GameMode = GameMode.Shared
-        });
-        Debug.Log(result);
-        Debug.Log($"SessionName:{networkRunner.SessionInfo.Name}");
-    }
+    public static bool isHost = false;
 
+    private async void Awake()
+    {
+        NetworkRunner runner = FindObjectOfType<NetworkRunner>();
+        if (runner == null || !runner.IsRunning)
+        {
+            isHost = true;
+            //prefab(ゲームオブジェクトの設計書)を使ってゲームオブジェクトをコピーする
+            runner = Instantiate(networkRunnerPrefab);
+            // ゲーム終了まで保持する
+            DontDestroyOnLoad(runner.gameObject);
+            var roomNumberText = FindObjectOfType<RoomNumberText>();
+            if (roomNumberText != null)
+            {
+                roomNumberText.SetRoomNumber();
+            }
+            //部屋を作る
+            var result = await runner.StartGame(new StartGameArgs
+            {
+                SessionName = RoomNumberText.roomNumStr,
+                GameMode = GameMode.Shared
+            });
+            Debug.Log(result);
+            Debug.Log($"生成したルームの名前:{runner.SessionInfo.Name}");
+        }
+        Debug.Log("ルームは生成されています。");
+    }
 }
